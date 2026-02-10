5905d0df7054f0cfd0aec367ecaacfc9e45ccfc1
zlgonzalez <zlgonzalez@yahoo.com>
2026-02-10 13:21:35 -0800

    Sitemap and robots.txt support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,27 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+import sitemapHttp from './src/integrations/sitemap-http';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    site: 'https://www.kubomontessori.com',
+    integrations: [
+        tailwind(),
+        sitemap(),
+        sitemapHttp(),
+        robotsTxt({
+            sitemap: [
+                'https://www.kubomontessori.com/sitemap-index.xml',
+                'https://www.kubomontessori.com/sitemap-http.xml'
+            ],
+            policy: [
+                {
+                    userAgent: '*',
+                    allow: '/',
+                },
+            ],
+        })
+    ],
 });
